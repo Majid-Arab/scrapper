e9ebe1c34b9da27efe960bef8f6a5e6ce6ae5289
Abdul-Majid <arabmajid12@gmail.com>
2025-01-11 12:51:22 +0500

    scrapped AzizBhatti

--- scrapper.py
+++ scrapper.py
@@ -278,7 +278,7 @@ def scrape_medical_businesses(api_key, shapefile_path, output_file, log_file='ap
 # Usage
 if __name__ == "__main__":
     api_key = os.getenv("GOOGLE_PLACES_API_KEY")
-    shapefile_path = "lahoreShp/Shahdara/Shahdara.shp"
-    output_file = "Shahdara.csv"
+    shapefile_path = "lahoreShp/AzizBhatti/AzizBhatti.shp"
+    output_file = "AzizBhatti.csv"
     
     scrape_medical_businesses(api_key, shapefile_path, output_file)
